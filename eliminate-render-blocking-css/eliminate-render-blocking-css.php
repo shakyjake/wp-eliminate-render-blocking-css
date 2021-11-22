0f0f83934f09cab82065f1b7276726d9470564da
shakyjake <6040147+shakyjake@users.noreply.github.com>
2021-11-22 11:50:29 +0000

    Update eliminate-render-blocking-css.php
    Add documentation. Add noscript fallback. Remove regex; we don't actually need it any more

--- eliminate-render-blocking-css/eliminate-render-blocking-css.php
+++ eliminate-render-blocking-css/eliminate-render-blocking-css.php
@@ -10,25 +10,28 @@ Author URI: https://github.com/shakyjake/
 Plugin URI: https://github.com/shakyjake/wp-eliminate-render-blocking-css/
 Requires at least: 4.5.0
 License: MIT
-Text Domain: erbc
 
 */
 
 function simple_defer_stylesheets($html, $handle, $href, $media){
 	
 	if(empty($media)){
-		$media = '';
+		$media = 'all';/* No "media" value == 'all' (undefined edition) */
+	}
+	
+	if(!is_string($media)){/* If it's not a string, somebody somewhere is doing something ridiculous, which we don't want to dignify */
+		return $html;
 	}
 	
 	if($media === ''){
-		$media = 'all';
+		$media = 'all';/* No "media" value == 'all' */
 	}
 	
-	if($media !== 'print'){
-		$media = preg_quote($media, "/");
-		$match = "/ media='" . $media . "'/";
-		$replace = ' media=\'print\' onload=\'this.media="' . $media . '"\'';
-		$html = preg_replace($match, $replace, $html);
+	if($media !== 'print'){/* No need to optimise print styles (tempting to discard print styles to help the environment but let's not) */
+		$match = " media='" . $media . "'";/* match the whole attribute declaration */
+		$replace = ' media=\'print\' onload=\'this.media="' . $media . '"\'';/* build a new media attribute declaration */
+		$html = str_replace($match, $replace, $html);/* swap old with new */
+		$html .= '<noscript><link rel="stylesheet" href="' . $href . '" media="' . $media . '" /></noscript>';/* append a noscript version so we don't break for clients without JS */
 	}
 
 	return $html;
